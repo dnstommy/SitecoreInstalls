e8f89863b20b8c6fbcd0672b3c0d62e913743ad6
Chris Auer <chris.auer@gmail.com>
2018-12-24 11:33:50 -0500

    Update 3-Install SIF Module.ps1

--- 9.1.0/Install/3-Install SIF Module.ps1
+++ 9.1.0/Install/3-Install SIF Module.ps1
@@ -1,1 +1,3 @@
-Register-PSRepository -Name SitecoreGallery_Internal -SourceLocation https://sitecore.myget.org/F/sc-powershell/api/v2Install-Module SitecoreInstallFramework -AllowPreRelease
+Register-PSRepository -Name SitecoreGallery_Internal -SourceLocation https://sitecore.myget.org/F/sc-powershell/api/v2
+
+Install-Module SitecoreInstallFramework
